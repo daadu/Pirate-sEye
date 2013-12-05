cf79cea89f469b1e53ea46ae4f12a36302a15f26
EXCELSIOR <harsh.bhikadiya@gmail.com>
2013-12-05 20:01:23 +0530

    added some css and removed few bug, looking preety

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var canvas = document.getElementById("canvas");
 canvas.width=600;canvas.height=480;
 var context = canvas.getContext("2d");
-var img = document.getElementById("image");
+var imglist = document.getElementById("imglist");
+//var img = document.getElementById("image");
 var video = document.getElementById("video");
 
 navigator.getUserMedia = navigator.getUserMedia ||navigator.webkitGetUserMedia ||navigator.mozGetUserMedia || navigator.msGetUserMedia;
@@ -23,7 +24,11 @@ function windowLoaded(){
 	}
 	function videoClicked(){
 		context.drawImage(video,0,0);
-		img.src = context.toDataURL();
+		var img = new Image();
+		img.onload = function(){
+			imglist.appendChild(img);
+		};
+		img.src = canvas.toDataURL();
 	}
 	function fail(){
 		alert("App not supported. Sorry!");
